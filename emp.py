65d2439f158130a91435793610ec1a05aa4320a6
unknown <keshav.wanjale@gmail.com>
2024-09-10 14:31:48 +0530

    [Keshav]: Program to chek Employee attendance

--- emp.py
+++ emp.py
@@ -1,1 +1,12 @@
-print("Welcome to employee wage computation.")
+import random
+
+def check_attendence():
+    attendence = random.randint(0,1)
+    if attendence == 1:
+        print("Employee is present")
+    else:
+        print("Employee is absent")
+
+if __name__  == "__main__":
+    print("Welcome to Employee Wage Program")
+    check_attendence()
