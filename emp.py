d18d5b9bafd1c3b79895c5adb79b5324a02cc385
unknown <keshav.wanjale@gmail.com>
2024-09-10 23:09:33 +0530

    [Keshav] Add: Python script for calculating wage till some conditions are satisfied

--- emp.py
+++ emp.py
@@ -6,20 +6,30 @@ def check_attendence():
 def cal_daily_wage():
     wage_per_hr = 20
     emp_attendance = check_attendence()
+    # returning tuples daily_wage, hours worked
     match emp_attendance:
         case 1:
-            return wage_per_hr * 8
+            return wage_per_hr * 8, 8
         case 2:
-            return wage_per_hr * 4
+            return wage_per_hr * 4, 4
         case _:
-            return 0
+            return 0,0
 
 def cal_monthly_wage():
     total_wage = 0
-    tota_days = 20
-    for i in range(tota_days):
-        total_wage += cal_daily_wage()
-    return total_wage
+    total_hours = 0
+    total_days = 0
+    max_hours = 100
+    max_days = 20
+    
+    while total_hours < max_hours and total_days < max_days:
+        #getting the tuples daily_wage, hours_worked
+        daily_wage, hours_worked = cal_daily_wage()
+        total_wage += daily_wage
+        total_hours += hours_worked
+        total_days += 1
+    
+    return total_wage #, total_hours,total_days
 
 if __name__  == "__main__":
     print("Welcome to Employee Wage Program")
