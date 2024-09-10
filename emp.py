1d67a6f5b3057cb9df225e43631b48d0b455acd6
unknown <keshav.wanjale@gmail.com>
2024-09-10 17:30:18 +0530

    [Keshav] Add: Python script for calculating monthly wage

--- emp.py
+++ emp.py
@@ -1,3 +1,16 @@
+  GNU nano 8.1                                        emp.py                                         Modified
+
+
+
+
+
+
+
+
+
+
+
+
 import random
 def check_attendence():
     attendence = random.randint(0,2)
@@ -8,15 +21,19 @@ def cal_daily_wage():
     emp_attendance = check_attendence()
     match emp_attendance:
         case 1:
-            daily_wage = wage_per_hr * 8
-            print(f"Employee was present full day so his daily wage is : {daily_wage}")
+            return wage_per_hr * 8
         case 2:
-            daily_wage = wage_per_hr * 4
-            print(f"Employee was present half day so his daily wage is : {daily_wage}")
+            return wage_per_hr * 4
         case _:
-            daily_wage = 0
-            print(f"Employee was absent for the day so his daily wage is : {daily_wage}")
+            return 0
+
+def cal_monthly_wage():
+    total_wage = 0
+    tota_days = 20
+    for i in range(tota_days):
+        total_wage += cal_daily_wage()
+    return total_wage
 
 if __name__  == "__main__":
     print("Welcome to Employee Wage Program")
-    cal_daily_wage()
+    print(f"Monthly wage of the employee is: {cal_monthly_wage()}")
