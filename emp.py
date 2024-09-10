53eec47c70a27dc623924b3600f18544830986fb
unknown <keshav.wanjale@gmail.com>
2024-09-10 17:04:54 +0530

    [Keshav] Add: Python script if employee works part time.

--- emp.py
+++ emp.py
@@ -1,7 +1,6 @@
 import random
-
 def check_attendence():
-    attendence = random.randint(0,1)
+    attendence = random.randint(0,2)
     return attendence
 
 def cal_daily_wage():
@@ -10,9 +9,14 @@ def cal_daily_wage():
     if emp_attendance == 1:
         daily_wage = wage_per_hr * 8
         print(f"Employee was present full day so his daily wage is : {daily_wage}")
+    elif emp_attendance == 2:
+        daily_wage = wage_per_hr * 4
+        print(f"Employee was present half day so his daily wage is : {daily_wage}")
     else:
         daily_wage = 0
         print(f"Employee was absent for the day so his daily wage is : {daily_wage}")
+
+
 if __name__  == "__main__":
     print("Welcome to Employee Wage Program")
     cal_daily_wage()
