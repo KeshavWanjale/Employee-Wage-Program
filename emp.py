fcf4d4f700a1a53316163e65eb3d97c453464e97
unknown <keshav.wanjale@gmail.com>
2024-09-10 17:23:44 +0530

    [Keshav] Add: Python script for calculating wage using match-case.

--- emp.py
+++ emp.py
@@ -6,16 +6,16 @@ def check_attendence():
 def cal_daily_wage():
     wage_per_hr = 20
     emp_attendance = check_attendence()
-    if emp_attendance == 1:
-        daily_wage = wage_per_hr * 8
-        print(f"Employee was present full day so his daily wage is : {daily_wage}")
-    elif emp_attendance == 2:
-        daily_wage = wage_per_hr * 4
-        print(f"Employee was present half day so his daily wage is : {daily_wage}")
-    else:
-        daily_wage = 0
-        print(f"Employee was absent for the day so his daily wage is : {daily_wage}")
-
+    match emp_attendance:
+        case 1:
+            daily_wage = wage_per_hr * 8
+            print(f"Employee was present full day so his daily wage is : {daily_wage}")
+        case 2:
+            daily_wage = wage_per_hr * 4
+            print(f"Employee was present half day so his daily wage is : {daily_wage}")
+        case _:
+            daily_wage = 0
+            print(f"Employee was absent for the day so his daily wage is : {daily_wage}")
 
 if __name__  == "__main__":
     print("Welcome to Employee Wage Program")
